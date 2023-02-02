a0cdd5fe25ada0a14ade16ed70a0f38c0d91f263
SpenserCai <spensercai@gmail.com>
2023-02-02 11:56:17 +0800

    init AltVLauncher

---
+++ scripts/AltVLauncher/go.mod
@@ -0,0 +1,3 @@
+module AltVLauncher
+
+go 1.19
---
+++ scripts/AltVLauncher/main.go
@@ -0,0 +1,11 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-02-02 11:55:11
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-02-02 11:55:27
+ * @Description: file content
+ */
+package main
+
+// atlv download:https://cdn.altv.mp/launcher/release/x64_win32/altv.zip
