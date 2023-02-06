08f5ba6f07adf5d7cb65686d1246e993e9049195
SpenserCai <spensercai@gmail.com>
2023-02-06 14:47:38 +0800

    test

--- scripts/AltVLauncher/go.mod
+++ scripts/AltVLauncher/go.mod
@@ -1,3 +1,7 @@
 module AltVLauncher
 
 go 1.19
+
+require github.com/SpenserCai/lgscore v0.0.2
+
+require github.com/andygrunwald/vdf v1.1.0 // indirect
--- scripts/AltVLauncher/main.go
+++ scripts/AltVLauncher/main.go
@@ -3,9 +3,22 @@
  * @Date: 2023-02-02 11:55:11
  * @version:
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-02-05 23:32:06
+ * @LastEditTime: 2023-02-06 14:01:32
  * @Description: file content
  */
 package main
 
+import (
+	"fmt"
+
+	"github.com/SpenserCai/lgscore"
+)
+
 // atlv download:https://cdn.altv.mp/launcher/release/x64_win32/altv.zip
+func main() {
+	v := lgscore.SteamApp{
+		AppId: "271590",
+	}
+	v.InitSteamApp()
+	fmt.Println(v)
+}
