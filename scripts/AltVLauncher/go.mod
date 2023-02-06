df97efd6e13faaa4dd97ef384a0542c59b3fe955
SpenserCai <spensercai@gmail.com>
2023-02-06 16:50:57 +0800

    InitAltV

--- scripts/AltVLauncher/go.mod
+++ scripts/AltVLauncher/go.mod
@@ -2,6 +2,6 @@ module AltVLauncher
 
 go 1.19
 
-require github.com/SpenserCai/lgscore v0.0.3
+require github.com/SpenserCai/lgscore v0.0.4
 
 require github.com/andygrunwald/vdf v1.1.0 // indirect
--- scripts/AltVLauncher/main.go
+++ scripts/AltVLauncher/main.go
@@ -3,16 +3,53 @@
  * @Date: 2023-02-02 11:55:11
  * @version:
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-02-06 14:51:24
+ * @LastEditTime: 2023-02-06 16:49:26
  * @Description: file content
  */
 package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/SpenserCai/lgscore"
+	lgsutils "github.com/SpenserCai/lgscore/utility"
 )
+
+var HomePath = os.Getenv("HOME")
+
+var AltvPath = HomePath + "/.LGS-Helper/altv"
+var AltvTmpPath = AltvPath + "/tmp"
+
+func InitAltV() error {
+	// 检查AltvPath是否存在,不存在则创建
+	if _, err := os.Stat(AltvPath); os.IsNotExist(err) {
+		os.MkdirAll(AltvPath, os.ModePerm)
+	}
+	// 检查AltvTmpPath是否存在,不存在则创建
+	if _, err := os.Stat(AltvTmpPath); os.IsNotExist(err) {
+		os.MkdirAll(AltvTmpPath, os.ModePerm)
+	}
+	// 检查AltvPath中是否存在altv.exe,不存在则下载
+	if _, err := os.Stat(AltvPath + "/altv.exe"); os.IsNotExist(err) {
+		downErr := new(lgsutils.NewWorkLGS).Download("https://cdn.altv.mp/launcher/release/x64_win32/altv.zip", AltvTmpPath+"/altv.zip")
+		if downErr != nil {
+			return downErr
+		}
+		// 解压
+		unzipedList := []string{}
+		unzipErr := new(lgsutils.FileLGS).UnZip(AltvPath+"/tmp/altv.zip", AltvPath, unzipedList)
+		if unzipErr != nil {
+			return unzipErr
+		}
+		// 删除临时文件
+		delErr := os.Remove(AltvTmpPath + "/altv.zip")
+		if delErr != nil {
+			return delErr
+		}
+	}
+	return nil
+}
 
 // atlv download:https://cdn.altv.mp/launcher/release/x64_win32/altv.zip
 func main() {
@@ -21,4 +58,5 @@ func main() {
 	}
 	gtavApp.InitSteamApp()
 	fmt.Println(gtavApp)
+	InitAltV()
 }
